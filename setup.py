a2b1f6bd6667b49ef4cb9603014027f289aaa7c2
Sergey Raevsky <sergey.raevsky@exactprosystems.com>
2023-02-08 01:40:45 +0400

    Update th2-grpc-common version

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ setup(
     license='Apache License 2.0',
     python_requires='>=3.7',
     install_requires=[
-        'th2-grpc-common>=3,<4',
+        'th2-grpc-common==4.1.0.dev4105266708',
         'mypy-protobuf==3.2'
     ],
     packages=packages,
